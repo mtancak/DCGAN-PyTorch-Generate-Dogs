6cbe48d4ba6a656fe2cc1e5cf8cbd962abb9b2f4
Milan Tancak <tancak.milan@gmail.com>
2022-04-10 17:18:21 +0100

    add some code that picks out images of dogs and displays them

--- extract_dog_class_cifar10.py
+++ extract_dog_class_cifar10.py
@@ -1,13 +1,23 @@
 import torch
 import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=None)
+                                            download=True, transform=transforms.ToTensor())
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=1,
                                               shuffle=False, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=None)
+                                           download=True, transform=transforms.ToTensor())
     testloader = torch.utils.data.DataLoader(testset, batch_size=1,
                                              shuffle=False, num_workers=2)
+
+    for data, label in trainloader:
+        if label.item() == 5:
+            print(data.numpy().shape)
+            plt.imshow(data[0].numpy().T)
+            plt.show()
+            print(label)

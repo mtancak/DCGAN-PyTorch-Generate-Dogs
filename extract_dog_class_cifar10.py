c68aabf2c07b6748e581726bdb5d1fe7e991e992
Milan Tancak <tancak.milan@gmail.com>
2022-04-10 19:41:09 +0100

    add logic to extract all dog images from the CIFAR10 dataset and save them as numpy files to a folder

--- extract_dog_class_cifar10.py
+++ extract_dog_class_cifar10.py
@@ -1,23 +1,41 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import os
+import numpy as np
+from tqdm import tqdm
 
-import matplotlib.pyplot as plt
+output_class = 5  # dogs = class 5
+
+work_dir = "."
+in_sub_dir = "data"
+out_sub_dir = "dogs"
 
 if __name__ == "__main__":
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    if not os.path.isdir(os.path.join(work_dir, out_sub_dir)):
+        os.mkdir(os.path.join(work_dir, out_sub_dir))
+
+    in_dir = os.path.join(work_dir, in_sub_dir)
+
+    train_set = torchvision.datasets.CIFAR10(root=in_dir, train=True,
+                                             download=True, transform=transforms.ToTensor())
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=1,
+                                               shuffle=False, num_workers=2)
+
+    test_set = torchvision.datasets.CIFAR10(root=in_dir, train=False,
                                             download=True, transform=transforms.ToTensor())
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=1,
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=1,
                                               shuffle=False, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transforms.ToTensor())
-    testloader = torch.utils.data.DataLoader(testset, batch_size=1,
-                                             shuffle=False, num_workers=2)
+    counter = 0
+    for data, label in tqdm(train_loader):
+        if label.item() == output_class:
+            out_path_file_name = os.path.join(work_dir, out_sub_dir, str(counter) + ".npy")
+            np.save(out_path_file_name, data.numpy())
+            counter += 1
 
-    for data, label in trainloader:
-        if label.item() == 5:
-            print(data.numpy().shape)
-            plt.imshow(data[0].numpy().T)
-            plt.show()
-            print(label)
+    for data, label in tqdm(test_loader):
+        if label.item() == output_class:
+            out_path_file_name = os.path.join(work_dir, out_sub_dir, str(counter) + ".npy")
+            np.save(out_path_file_name, data.numpy())
+            counter += 1
